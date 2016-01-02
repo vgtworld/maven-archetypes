3db52fb7b8ec7643a930edfb239a9d7c31a2f767
VGT <tomek@vgtworld.pl>
2016-01-03 00:12:45 +0100

    Add core classes: PersistenceUtil and jsp tags.

--- jee-archetype/src/main/resources/archetype-resources/src/main/java/app/hello/HelloController.java
+++ jee-archetype/src/main/resources/archetype-resources/src/main/java/app/hello/HelloController.java
@@ -1,11 +1,13 @@
 package ${package}.app.hello;
 
 import com.googlecode.htmleasy.View;
+import ${package}.app.hello.models.HelloModel;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Date;
 
 @Path("/")
 public class HelloController {
@@ -13,7 +15,9 @@ public class HelloController {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public View getResourceList() {
-		return new View("/views/hello.jsp", null);
+		HelloModel model = new HelloModel();
+		model.setCurrentDate(new Date());
+		return new View("/views/hello.jsp", model);
 	}
 
 }
