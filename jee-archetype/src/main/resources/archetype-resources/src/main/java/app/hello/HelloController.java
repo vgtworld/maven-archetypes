4759d23fd0c6a28bb06f2aa70fe51021dc3f257c
VGT <tomek@vgtworld.pl>
2016-02-24 22:12:14 +0100

    Add basic model and controller and use it in view.

--- jee-archetype/src/main/resources/archetype-resources/src/main/java/app/hello/HelloController.java
+++ jee-archetype/src/main/resources/archetype-resources/src/main/java/app/hello/HelloController.java
@@ -1,23 +1,26 @@
 package ${package}.app.hello;
 
-import com.googlecode.htmleasy.View;
-import ${package}.app.hello.models.HelloModel;
+import pl.vgtworld.test.core.app.hello.models.HelloModel;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 import java.util.Date;
 
-@Path("/")
+@Named
+@RequestScoped
 public class HelloController {
 
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public View getResourceList() {
-		HelloModel model = new HelloModel();
+	private HelloModel model;
+
+	public HelloModel getModel() {
+		return model;
+	}
+
+	@PostConstruct
+	public void init() {
+		model = new HelloModel();
 		model.setCurrentDate(new Date());
-		return new View("/views/hello.jsp", model);
 	}
 
 }
