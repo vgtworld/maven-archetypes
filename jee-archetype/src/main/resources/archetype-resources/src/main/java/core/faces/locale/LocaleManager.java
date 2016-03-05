4a918cd94ae19e3807bcae605958f6bf65bd6d45
VGT <tomek@vgtworld.pl>
2016-03-05 16:52:57 +0100

    Add available languages to LocaleManager.

--- jee-archetype/src/main/resources/archetype-resources/src/main/java/core/faces/locale/LocaleManager.java
+++ jee-archetype/src/main/resources/archetype-resources/src/main/java/core/faces/locale/LocaleManager.java
@@ -8,13 +8,25 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 @Named
 @SessionScoped
 public class LocaleManager implements Serializable {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(LocaleManager.class);
+
+	public static final String LANG_EN = "en";
+
+	public static final String LANG_PL = "pl";
+
+	private static final List<String> AVAILABLE_LANGUAGES = Arrays.asList(LANG_EN, LANG_PL);
+
+	private static final String DEFAULT_LANGUAGE = LANG_EN;
 
 	private Locale activeLocale;
 
@@ -26,16 +38,33 @@ public class LocaleManager implements Serializable {
 		return activeLocale.getLanguage();
 	}
 
-	public void setActiveLanguage(String locale) {
-		LOGGER.debug("Change session locale. locale:{}", locale);
-		activeLocale = new Locale(locale);
+	public void setActiveLanguage(String language) {
+		LOGGER.debug("Change session locale. language:{}", language);
+		if (!AVAILABLE_LANGUAGES.contains(language)) {
+			LOGGER.warn("Language not available. Unable to change locale. language:{}", language);
+			return;
+		}
+		activeLocale = new Locale(language);
 		FacesContext.getCurrentInstance().getViewRoot().setLocale(activeLocale);
+	}
+
+	public Map<String, String> getAvailableLanguages() {
+		Map<String, String> availableLanguages = new LinkedHashMap<>();
+		for (String availableLanguage : AVAILABLE_LANGUAGES) {
+			availableLanguages.put(availableLanguage.toUpperCase(), availableLanguage);
+		}
+		return availableLanguages;
 	}
 
 	@PostConstruct
 	public void init() {
 		Locale requestLocale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
 		LOGGER.debug("Setting session locale based on request. locale:{}", requestLocale);
+		if (!AVAILABLE_LANGUAGES.contains(requestLocale.getLanguage())) {
+			LOGGER.debug("Language not available. Switching to default locale.");
+			activeLocale = new Locale(DEFAULT_LANGUAGE);
+			return;
+		}
 		activeLocale = requestLocale;
 	}
 
